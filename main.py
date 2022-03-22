d64354fe521658fd9b59cd187d221469794f6cf6
Vasiliev Daniil <vasiliev.daniil.o@gmail.com>
2022-03-22 13:39:16 +0300

    step 0: setting environment and connect all use fraemwork

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+
+from db.base import database
+
+app = FastAPI()
+
+@app.get("/")
+async def root ():
+    return {"message":"Hello my first api web-site"}
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+
+if __name__ ==  "__main__":
+    uvicorn.run("main:app", port = 8000, host = "0.0.0.0", reload = True)
